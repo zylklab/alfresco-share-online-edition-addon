7adc1ebe4786fb32fb4a61c43dfd4ce7a552daf6
patricia.zylk <patricia.zylk@b4e9b488-f51f-48cd-1867-095c99c8c87f>
2013-05-16 06:20:21 +0000

    Initial commit

---
+++ trunk/META-INF/components/online-edition/zk-online-edition-actions.js
@@ -0,0 +1,22 @@
+(function() {
+    YAHOO.Bubbling.fire("registerAction",
+    {
+        actionName: "onActionZKEditOnline",
+        fn: function ZK_onActionZKEditOnline(file) {
+        	
+        	var webdavPath = "";
+        	if(navigator.userAgent.search("Firefox") >= 0){
+          	  var davProtocol = "dav";
+          	  if(window.location.protocol.indexOf("https") != -1){
+        		  davProtocol = "davs";
+        	  }
+          	  webdavPath = davProtocol+"://"+ window.location.host + "/alfresco"+ file.webdavUrl;
+            }else {
+            	webdavPath = window.location.protocol+"//"+ window.location.host + "/alfresco"+ file.webdavUrl;
+            }
+            
+        	
+        	window.location = webdavPath;
+        }
+    });
+})();
