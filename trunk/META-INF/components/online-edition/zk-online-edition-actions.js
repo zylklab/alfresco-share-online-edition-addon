0095dfea64ef18005a49609b763776e8d5e075ef
cesar.capillas@gmail.com <cesar.capillas@gmail.com@b4e9b488-f51f-48cd-1867-095c99c8c87f>
2013-12-16 23:57:25 +0000

    Let dav:// or davs:// urls for all browsers, also for IE and Chrome, which can be configured registering the protocol in the operating system.

--- trunk/META-INF/components/online-edition/zk-online-edition-actions.js
+++ trunk/META-INF/components/online-edition/zk-online-edition-actions.js
@@ -5,15 +5,15 @@
         fn: function ZK_onActionZKEditOnline(file) {
         	
         	var webdavPath = "";
-        	if(navigator.userAgent.search("Firefox") >= 0){
+        	//if(navigator.userAgent.search("Firefox") >= 0){
           	  var davProtocol = "dav";
           	  if(window.location.protocol.indexOf("https") != -1){
         		  davProtocol = "davs";
-        	  }
+        	    }
           	  webdavPath = davProtocol+"://"+ window.location.host + "/alfresco"+ file.webdavUrl;
-            }else {
-            	webdavPath = window.location.protocol+"//"+ window.location.host + "/alfresco"+ file.webdavUrl;
-            }
+          //  }else {
+          //  	webdavPath = window.location.protocol+"//"+ window.location.host + "/alfresco"+ file.webdavUrl;
+          //  }
             
         	
         	window.location = webdavPath;
